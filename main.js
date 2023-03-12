cbcd0ceb3b28b7d5ab8088de39d0417bbd9fc3cd
javohir009 <abdumalikovjavohir009@gmail.com>
2023-03-12 13:44:34 +0300

    site loader

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener(`DOMContentLoaded`, () =>{
+    const loader =document.querySelector('.loader')
+    setTimeout(() =>{
+        loader.style.opacity ="0"
+        loader.style.display ="none"
+    }, 2000)
+})
